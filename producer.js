4943091b497f6134af98df3555739bb89b98fc5e
im23b-terenzianie <terenzianie@bzz.ch>
2024-12-03 16:19:45 +0100

    Producer.html und producer.css erstellt und erledigt. Aktienkurse müssen noch hinzugefügt werden

---
+++ producer.js
@@ -0,0 +1,8 @@
+function burgermenue() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
